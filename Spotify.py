633f964db42a2c7cf502d2b76a36ef1c695e3a6d
Gabriel-Quispe <107212590+Gabriel-Quispe@users.noreply.github.com>
2022-06-16 16:08:22 -0300

    Nueva funcion de Spotify
    Agregue la función para listar playlists de spotify que había en el archivo "Listar_Playlist_Spotify.py"

--- Spotify.py
+++ Spotify.py
@@ -6,7 +6,7 @@ from tekore import Spotify
 CLIENT_ID = '9446fa59f25043cda0f3162443491415'
 CLIENT_SECRET = 'ddb6635b0f8b4d999e499172db01d5f6'
 
-def acceder_al_servicio()->Spotify:
+def Generar_Servicio_Spotify()->Spotify:
 
 	token: RefreshingToken = None
 	configuracion = (CLIENT_ID,CLIENT_SECRET,'https://example.com/callback')
@@ -22,3 +22,24 @@ def acceder_al_servicio()->Spotify:
 		tk.config_to_file('tekore.cfg', configuracion + (token.refresh_token,))
 
 	return tk.Spotify(token)
+
+def Listar_Playlist_Spotify( spotify : Spotify) -> None:
+
+	user = spotify.current_user()
+
+	total_playlist = spotify.playlists(user.id).total
+
+	for i in range(total_playlist):
+
+		titulo = spotify.playlists(user.id).items[i].name
+		print(f"{titulo}:")
+
+		playlist_id = spotify.playlists(user.id).items[i].id
+		total_canciones =  spotify.playlist_items(playlist_id).total
+
+		for j in range(total_canciones):
+
+			nombre_cancion = spotify.playlist_items(playlist_id).items[j].track.name 
+			print(f"	{nombre_cancion}")
+
+		print("-------------------------------")
